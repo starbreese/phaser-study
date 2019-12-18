c6a4e7e58e48531a1e5c4769a5626527b3700ff7
admin <admin@TJSH100026.local>
2019-12-18 10:18:07 +0800

    first commit

---
+++ src/scenes/LoadScene.js
@@ -0,0 +1,17 @@
+import { CST } from "../CST"
+export class LoadScene extends Phaser.Scene{
+  constructor() {
+    super({
+      key: CST.SCENES.LOAD
+    })
+  }
+  init() {
+
+  }
+  preload() {
+
+  }
+  create() {
+    this.scene.start(CST.SCENES.MENU, "hello phaser")
+  }
+}
---
+++ src/scenes/MenuScene.js
@@ -0,0 +1,17 @@
+import { CST } from "../CST"
+export class MenuScene extends Phaser.Scene{
+  constructor() {
+    super({
+      key: CST.SCENES.MENU
+    })
+  }
+  init(data) {
+    console.log(data)
+    console.log('I Got it')
+  }
+  preload() {
+
+  }
+  create() {
+  }
+}
